9e39306b034d6a4d884cea7e1e572aa3b20cdb00
jack <loaizajack@gmail.com>
2022-06-28 21:59:21 +0100

    showing badge options

--- src/stories/Badge.stories.jsx
+++ src/stories/Badge.stories.jsx
@@ -1,18 +1,26 @@
 import Badge from "../Badge";
 import {setGlobalStyles} from "../helpers";
+import {styled, theme} from "../../stitches.config";
 
 export default {
     title: 'Display/Badge',
     component: Badge
 }
 
-
 const Template = (args) => {
     setGlobalStyles();
+
+    const FlexGrid = styled('div',{
+        display: 'flex',
+        gap: theme.space.reg
+    })
+
     return (
-        <>
+        <FlexGrid>
             <Badge value={44}/>
-        </>
+            <Badge value={900}/>
+            <Badge value={111} limit={999}/>
+        </FlexGrid>
     )
 };
 
